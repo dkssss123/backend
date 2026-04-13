042993d2b9277ee50df6faf36a1b0e5f00b7d558
dkssss123 <dharam.kittu2021@gmail.com>
2026-04-13 15:49:00 +0530

    cloudinary and multer update

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,11 +1,30 @@
 import { v2 as cloudinary } from "cloudinary"
+import { response } from "express"
 import fs from "fs"
 
 
 
     // Configuration
     cloudinary.config({ 
-        cloud_name: '', 
-        api_key: '', 
-        api_secret: '' // Click 'View API Keys' above to copy your API secret
-    }) 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    })  
+
+    const uploadOnCloudinary = async (localfilePath) => {
+        try {
+            if (!localfilePath) return null
+            // Upload file to Cloudinary
+            const responce = await cloudinary.uploader.upload(localfilePath, {
+                resource_type: "auto",
+            }) 
+            // file has been uploaded successfully
+            console.log("File uploaded on cluoudinary successfully",response.url)
+            return response 
+        } catch (error) {
+            fs.unlinkSync(localfilePath)// remove the locally saved temporary file as the upload operation has failed
+            return null
+        } 
+
+    }
+    export {uploadOnCloudinary}
